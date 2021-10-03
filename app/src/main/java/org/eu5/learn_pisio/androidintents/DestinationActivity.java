04993984c2d283d0358c64aca8453948403d9c00
Dragan Cajic <cajic_dragan@yahoo.com>
2021-10-03 21:34:46 +0200

    First Commit

---
+++ app/src/main/java/org/eu5/learn_pisio/androidintents/DestinationActivity.java
@@ -0,0 +1,20 @@
+package org.eu5.learn_pisio.androidintents;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DestinationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_destination);
+
+        TextView values = (TextView)findViewById(R.id.txtValues);
+
+        // TODO: extract any data passed by the caller
+
+    }
+}
